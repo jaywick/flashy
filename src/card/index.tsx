be831d40b115288e3535b1c7669138811052c1d2
Jay Wick <dev@jaywick.xyz>
2019-11-22 00:46:44 +1100

    Initial card

---
+++ src/card/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './styles.css'
+
+export const Card = () => {
+    return (
+        <div className='card'>
+            <div className='question'>What type is an array?</div>
+        </div>
+    )
+}
