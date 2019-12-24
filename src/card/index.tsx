acc906ed4665d2b7ccd2c4cc9e18d81e4a5ee8be
Jay Wick <dev@jaywick.xyz>
2019-12-24 12:38:40 +1100

    Stylised card

--- src/card/index.tsx
+++ src/card/index.tsx
@@ -4,7 +4,13 @@ import './styles.css'
 export const Card = () => {
     return (
         <div className='card'>
-            <div className='question'>What type is an array?</div>
+            <div className='question'>
+                <h1>What type is an array?</h1>
+            </div>
+            <div className='answer'>
+                <h1>An object.</h1>
+                <p>Can be considered a subtype of an object</p>
+            </div>
         </div>
     )
 }
